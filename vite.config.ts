ec3610f2aadeff5c1985b591ad0f4dac45f3e28d
im_abramon <shelopugin.vlad@yandex.ru>
2024-09-05 21:52:34 +0300

    тест

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import fs from 'fs'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,12 +9,15 @@ export default defineConfig({
     port: 443,
     host: "0.0.0.0",
     hmr: {
-        host: 'localhost',
-        port: 443,
+      host: "localhost",
+      port: 443,
     },
-    https: {
-      key: fs.readFileSync('./.cert/localhost-key.pem'),
-      cert: fs.readFileSync('./.cert/localhost.pem'),
-    },
+    https:
+      import.meta.env.MODE === "development"
+        ? {
+            key: fs.readFileSync("./.cert/localhost-key.pem"),
+            cert: fs.readFileSync("./.cert/localhost.pem"),
+          }
+        : {},
   },
-})
+});
