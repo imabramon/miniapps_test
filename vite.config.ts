be3b3d34ef26116d72d4bc3d6fb5b9630d7607c4
im_abramon <shelopugin.vlad@yandex.ru>
2024-09-05 21:58:25 +0300

    Робот помог

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,23 @@ import react from "@vitejs/plugin-react";
 import fs from "fs";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 443,
-    host: "0.0.0.0",
-    hmr: {
-      host: "localhost",
+export default defineConfig(({ mode }) => {
+  const isDev = mode === "development";
+  return {
+    plugins: [react()],
+    server: {
       port: 443,
-    },
-    https:
-      import.meta.env.MODE === "development"
+      host: "0.0.0.0",
+      hmr: {
+        host: "localhost",
+        port: 443,
+      },
+      https: isDev
         ? {
             key: fs.readFileSync("./.cert/localhost-key.pem"),
             cert: fs.readFileSync("./.cert/localhost.pem"),
           }
-        : {},
-  },
+        : false,
+    },
+  };
 });
