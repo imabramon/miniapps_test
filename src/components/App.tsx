f067be9d5e028e7a729c71838ada85f9f080fc5f
im_abramon <shelopugin.vlad@yandex.ru>
2024-09-09 10:59:08 +0300

    Пробую убрать закрытие при свайпе вниз

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,20 @@
 import { FC } from "react";
 import ZodiacWheel from "./ZodiacWheel";
 
+declare global {
+  interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    Telegram: any;
+  }
+}
+
 const App: FC = () => {
+  try {
+    window.Telegram.WebApp.isVerticalSwipesEnabled = false;
+  } catch {
+    console.log("something went wrong");
+  }
+
   return (
     <ZodiacWheel
       onSelectSign={(sign) => {
